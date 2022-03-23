5fa92c63f5c9b37b73a57e98aa360ada87fbc68c
Heloise Cuche <heloise.cuche@heig-vd.ch>
2022-03-23 16:00:20 +0100

    add file csv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import * as d3 from 'd3'
-
-// Pour importer les données
-// import file from '../data/data.csv'
+import allMeteorites from '/data/meteorite-landings.csv'
 
 
 
+//year must be between 2006-2016
+const meteorites = []
+allMeteorites.forEach(meteorite => {
+    if(meteorite.year > 2006 && meteorite.year < 2016){
+        meteorites.push(meteorite)
+    }
+})
+console.log(meteorites)
 
+//sort by year
+// for (let i = 2006; i <= 2016; i++) {
+//     meteorites.forEach(meteorite => {
+//         if(meteorite.year == 2006){
+//             compteur = compteur + 1
+//         }
+//     });
+//     console.log("test")
+    
+// }
+// console.log(compteur)
