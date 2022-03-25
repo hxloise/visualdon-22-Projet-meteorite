f1c4fe150e3ff1662ba0b4c01e47b89a01dd92d6
Heloise Cuche <heloise.cuche@heig-vd.ch>
2022-03-25 14:26:32 +0100

    first step for graph

--- src/index.js
+++ src/index.js
@@ -5,23 +5,78 @@ import allMeteorites from '/data/meteorite-landings.csv'
 //year must be between 2006-2016
 const meteorites = []
 allMeteorites.forEach(meteorite => {
-    if (meteorite.year > 2006 && meteorite.year < 2016) {
+    if (meteorite.year >= 2006 && meteorite.year <= 2013) {
         meteorites.push(meteorite)
     }
 })
+
+
 console.log(meteorites)
+
+//test affichage 
+d3.select("body")
+    .append("div")
+    .attr('id', 'div-number-meteorite')
+
+
+
+//graph number of meteorite
+d3.select("body #Statistics")
+    .append("div")
+    .attr('id', 'graph-meteorite-numbers')
+
+
+
+
 
 //year chosen by user 
 let yearChoose = 2006
 const label = document.getElementById('year-select')
+
 label.addEventListener('change', function () {
     yearChoose = label.value
-    //sort by year
+    //count number of meteorite
     let compteur = 0
     allMeteorites.forEach(meteorite => {
         if (meteorite.year == yearChoose) {
             compteur = compteur + 1
         }
     })
+
+
+    //draw with data
+    const width = 1000
+    const height = 450
+    const margin = { top: 50, bottom: 50, left: 50, right: 0 }
+
+
+    let svg = d3.select("#graph-meteorite-numbers")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+
+
+    //define scale for axe x
+    let x = d3.scaleBand()
+    .domain(yearChoose) 
+    .range([0, 1])
+
+    //define scale for axe y
+    let y = d3.scaleLinear()
+    .domain([0, 10])
+    .range([height, 0])
+
+
+
+
+
+    //just test something 
     console.log(compteur)
+    d3.select('#div-number-meteorite')
+        .append('p')
+        .text(`En ${yearChoose}, il y a eu  ${compteur} météorites qui sont tombées`)
 })
+
